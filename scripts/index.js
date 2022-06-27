d0eefc3c4f6040975f030636b1b002a2b79f14ea
Ëvgenii Golbraikh <evgeniigolbraikh@gmail.com>
2022-06-27 19:25:40 +0300

    Рефакторинг после 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -15,19 +15,25 @@ let close = popup.querySelector('.popup__close-button');
 // Открываем попап
 function openPopup() {
   popup.classList.add('popup_opened');
+  // Присваиваем инпутам значения из профиля
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
 }
 
 // Закрываем попап
 function closePopup() {
   popup.classList.remove('popup_opened');
+  // Оставляем инпутам старые значения
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
 }
 
-// Присваиваем полям введенные значения и закрываем попап
+// Присваиваем полям новые значения и закрываем попап
 function updateFields(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  closePopup();
+  popup.classList.remove('popup_opened');
 }
 
 // Запускаем попап при нажатии на кнопку редактирования
